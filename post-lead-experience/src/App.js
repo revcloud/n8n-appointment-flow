8c1591535d514710b8d854477da251d8d2e8749e
Mubashar <mubasharjaved3567@gmail.com>
2025-09-17 12:46:33 +0500

    experience 2

--- post-lead-experience/src/App.js
+++ post-lead-experience/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import ThankYouPage from './experiences/ThankYouPage';
+import ThankYouPageWithAvailability from './experiences/ThankYouPageWithAvailability';
 
 function App() {
-  return (
-    <div className="App">
-      <ThankYouPage />
-    </div>
-  );
+  // Get experience ID from URL parameters or default to 1
+  const urlParams = new URLSearchParams(window.location.search);
+  const experienceId = urlParams.get('plid') || 1;
+  
+  // Render component based on experienceId using switch case
+  switch (experienceId) {
+    case 2:
+    case '2':
+      return (
+        <div className="App">
+          <ThankYouPageWithAvailability />
+        </div>
+      );
+    
+    case 1:
+    case '1':
+    default:
+      return (
+        <div className="App">
+          <ThankYouPage />
+        </div>
+      );
+  }
 }
 
 export default App;
